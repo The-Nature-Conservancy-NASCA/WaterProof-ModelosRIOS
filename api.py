86e88b3b43ec0c022e9c0a1cf56e00cabb1afad2
Edwin Alberto Piragauta Vargas <eapiragautav@dane.gov.co>
2021-05-11 15:40:41 -0500

    Change base api_py_3

--- api.py
+++ api.py
@@ -108,8 +108,8 @@ def execPreproc():
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
     }
 
-    # base_url_api = 'http://dev.skaphe.com:8000/'
-    base_url_api = 'http://wfapp_py3_container:8000/'
+    base_url_api = 'http://dev.skaphe.com:8000/'
+    #base_url_api = 'http://wfapp_py3_container:8000/'
     url = base_url_api + 'cobTrans'
 
     first_nbs = nbsList[0]
