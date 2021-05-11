29328d89d24eb4ca9108144f784f0b7617cdc896
Edwin Alberto Piragauta Vargas <eapiragautav@dane.gov.co>
2021-05-11 14:34:19 -0500

    set first nbs from list in cobtrans

--- api.py
+++ api.py
@@ -112,9 +112,10 @@ def execPreproc():
     base_url_api = 'http://wfapp_py3_container:8000/'
     url = base_url_api + 'cobTrans'
 
+    first_nbs = nbsList[0]
     parameters = {
         'pathCobs': process_path + '/out/04-RIOS/1_investment_portfolio_adviser_workspace/activity_portfolios/continuous_activity_portfolios',
-        'nbs_id': 5,
+        'nbs_id': first_nbs,
         'pathLULC': process_path + '/in/04-RIOS/LULC_SA_1.tif'
     }
 
