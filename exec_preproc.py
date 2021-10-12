ae51aea27e3b7a2ffe222a1d875d08616ad8b983
Edwin Alberto Piragauta Vargas <edwin.piragauta@gmail.com>
2021-10-12 10:56:52 -0500

    some refactors
    Signed-off-by: Edwin Alberto Piragauta Vargas <edwin.piragauta@gmail.com>

--- exec_preproc.py
+++ exec_preproc.py
@@ -24,6 +24,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from AdvancedHTMLParser import AdvancedTag
 from rasterio.mask import mask
+from execRIOS import getParameters,processParameters,execModel
 from zonalStatistics import calculateRainfallDayMonth, calculateStatistic
 from createBioParamCsv import getColsParams, generateCsv, getDefaultBiophysicParams,getUserBiophysicParams
 
@@ -399,50 +400,7 @@ def processParameters(parametersList, basin,id_catchment, studyCase,catchment, p
             value = file
         dictParameters[name] = value
         # print(parameter)
-
-    # for parameter in parametersList:
-        # name = parameter[0]
-        # value = parameter[1]
-        # if(value == 'False'):
-        #     value = False
-        # elif(value == 'True'):
-        #     value = True
-        # cut = parameter[2]
-        # constant = parameter[3]
-        # suffix = parameter[4]
-        # empty = parameter[5]
-        # file = parameter[6]
-        # folder = parameter[7]
-        # outPathType = parameter[8]
-        # calculado = parameter[11]
-        # inputUser = parameter[12]
-        # if(suffix):
-        #     region = getRegionFromId(basin)
-        #     label = region[4]
-        #     value = label
-        # if(constant):
-        #     constantValue = getConstantFromBasin(basin,name)
-        #     value = constantValue[2]
-        # if(empty):
-        #     value = ''
-        # if(cut):
-        #     value = cutRaster(catchment,value,in_path)
-        # if(file):
-        #     value = catchment
-        # if(outPathType):
-        #     value = out_path
-        # # if(calculado):
-        #     # region = getRegionFromId(basin)
-        #     # label = region[4]
-        #     # maxMonth,outRaster = calculateRainfallDayMonth(value,catchment,label)
-        #     # value = cutRaster(catchment,outRaster,in_path)
-        # # if(inputUser):
-        # #     value = inputs[name]
-
-        # dictParameters[name] = value
-        # print(value)
-
-
+    
     logger.debug("processParameters :: end")    
     return dictParameters, out_path, catchment_out,maxMonth
 
@@ -455,15 +413,11 @@ def executeFunction(basin, id_catchment, id_usuario, inputs,id_case,catchmentDir
     print ("id_case ::: %s" %id_case)
     print ("catchmentDir ::: %s" %catchmentDir)
     today = datetime.date.today()
-    # path = os.path.join("/home/skaphe/Documentos/tnc/modelos/Workspace_BasinDelineation/tmp",str(id_usuario) +  "_" + str(date.year) + "_" + str(date.month) + "_" + str(date.day))
-    # path = os.path.join("data","wpdev","salidas",str(id_usuario) +  "_" + str(date.year) + "_" + str(date.month) + "_" + str(date.day))
     path = os.path.join(ruta, "salidas", "%s_%s_%s-%s-%s" % (int(id_usuario), int(id_case), today.year, today.month, today.day))
     pathPreprocIn = os.path.join(path,catchmentDir, "in", "02-PREPROC_RIOS")
     pathPreprocOut = os.path.join(path,catchmentDir, "out", "02-PREPROC_RIOS")
     pathCatchment = os.path.join(path,catchmentDir, "in", "catchment")
-
-    #print(inputs)
-
+    
     isdir = os.path.isdir(path)
     if(not isdir):
         os.mkdir(path)
@@ -534,37 +488,6 @@ def executeFunction(basin, id_catchment, id_usuario, inputs,id_case,catchmentDir
     logger.debug("Pro.main :: END")
     print ("finish ::: executeFunction")
     return objectives, parameters["output_path"], catchmentOut,pcp_label
-
-# def executeFunction(basin,model,type,id_catchment,id_usuario):
-# 	date = datetime.date.today()
-# 	path = createFolder(id_usuario,date)
-
-# 	list = getParameters(basin,model)
-# 	catchment = exportToShp(id_catchment, path)
-# 	parameters,pathF,label = processParameters(list,basin,catchment,path,type,model)
-
-
-# 	if(model == 'awy'):
-# 		awy.execute(parameters)
-# 	elif(model == 'sdr'):
-# 		sdr.execute(parameters)
-# 	elif(model == 'carbon'):
-# 		carbon.execute(parameters)
-# 	elif(model == 'ndr'):
-# 		ndr.execute(parameters)
-# 	elif(model == 'swy'):
-# 		swy.execute(parameters)
-
-# 	return catchment,path,label
-
-
-# listP = getParameters(26,'preprocRIOS')
-# inputs = {"do_erosion":True,"do_nutrient_p":True,"do_nutrient_n":True,"do_flood":True,"do_gw_bf":True}
-# # catchment = exportToShp([6], "/home/skaphe/Documentos/tnc/modelos/Workspace_BasinDelineation/tmp/9_2020_10_24/")
-# # parameters,out_path = processParameters(listP,26,catchment,"/home/skaphe/Documentos/tnc/modelos/Workspace_BasinDelineation/tmp/9_2020_10_24/",inputs)
-# # print(out_path)
-# # print(parameters)
-# executeFunction(44,[3],1,inputs)
 
 def analysisPeriodFromStudyCase(id):
     logger.debug("analysisPeriodFromStudyCase - id::%s" % id)
@@ -667,7 +590,6 @@ def parse_to_get_ipa_report(path_file,catchment,id_case,id_usuario):
     conn.close()
     return "Ipa Report Ready"
 
-
 def updateStudyCaseRunAnalisys(id):
     conn = connect('postgresql_alfa')
     cursor = conn.cursor()
@@ -690,7 +612,6 @@ def queryStudyCaseRunAnalisys(id):
     cursor.close()
     conn.close()
     return result
-
 
 def sendEmail(id_user, study_case_id, start):
 
@@ -876,3 +797,278 @@ def makeGetRequest(url, parameters, timeout, headers):
     r = requests.get(url=url, params=parameters)
     data = r.json()
     return data
+
+def preproc_rios(id_usuario, id_case):
+
+  logging.info("*** preproc_rios :: START ***")
+  reload(sys)  # Reload is a hack
+  sys.setdefaultencoding('UTF8')
+    
+  sendEmail(id_usuario, id_case, True)
+
+  studyCases_objectives = getStudyCaseObjectives(id_case)
+  result = {'message': 'Preprocessing', 'status': 'success'}
+  
+  objectives={
+      'do_erosion':True,
+      'do_nutrient_p': True,
+      'do_nutrient_n':True,
+      'do_flood': True,
+      'do_gw_bf': True
+  }
+  
+  do_erosion =  objectives['do_erosion']
+  do_np = objectives['do_nutrient_p']
+  do_nn = objectives['do_nutrient_n']
+  do_flood = objectives['do_flood']
+  do_gw_bf = objectives['do_gw_bf']
+  
+  logging.debug('debug message')
+  inputs = {"do_erosion": bool(do_erosion), "do_nutrient_p": bool(do_np), "do_nutrient_n": bool(
+      do_nn), "do_flood": bool(do_flood), "do_gw_bf": bool(do_gw_bf)}
+  catchments = getStudyCaseCatchments(id_case)
+  ptapCatchments=getPtapCatchmentsByStudyCase(id_case)
+  catchments=list(set(catchments+ptapCatchments))
+  nbsList = getStudyCaseNbs(id_case)
+  ptaps=getStudyCasePtaps(id_case)
+  catchmentList = []
+  ptapList=[]
+  base_url_api = 'http://wfapp_py3:8000/wf-models/'
+  #base_url_api = 'http://dev.skaphe.com:8000/'
+  headers = {
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+  }
+  #------------------------#
+  # EJECUCION EXCHANGE RATE
+  #------------------------#
+  urlExchageRate = base_url_api + 'exchangeRate'
+  parameters = {
+      'study_case_id': id_case
+  }
+  data = makeGetRequest(urlExchageRate, parameters, 5, headers)
+  for catch in catchments:
+      catchmentList.append(catch[0])
+  for ptap in ptaps:
+      ptapList.append(ptap[0])
+  for counter, catchment in enumerate(catchmentList):
+      catchment=str(catchment)
+      basinQuery = getCatchmentBasin(catchment)
+      basin = str(basinQuery[0])
+      catchmentDir='WI_'+catchment
+      today = datetime.date.today()
+      out_directory = "%s_%s_%s-%s-%s/%s" % (int(id_usuario), int(id_case), today.year, today.month, today.day,catchmentDir)
+      print(":::BASIN::: %s" % basin)
+      
+      obj, outputPath, catchmentOut,pcp_label = executeFunction(basin, catchment, id_usuario, inputs,id_case,catchmentDir)
+      list_parameters = getParameters(basin, 'rios')
+      print("::CATCHMENT OUT:::")
+      print(catchmentOut)
+      listObjs = []
+      #Asignar lista de objetivos del CE
+      for objective in studyCases_objectives:
+          listObjs.append(objective[0])
+
+      process_path = "/home/skaphe/Documentos/tnc/modelos/salidas/%s/" % (out_directory)
+      # process_path = "/data/outputs/%s/" % (out_directory)
+      isdir = os.path.isdir(process_path)
+      if(not isdir):
+          os.mkdir(process_path)
+
+      isdir = os.path.isdir(process_path + 'out')
+      if(not isdir):
+          os.mkdir(process_path + 'out')
+
+      isdir = os.path.isdir(process_path + 'in')
+      if(not isdir):
+          os.mkdir(process_path + 'in')
+      
+      parameters, out_path = processParameters(
+          nbsList,list_parameters, catchment,id_case,basin, process_path, id_usuario, listObjs, obj, outputPath, catchmentOut,pcp_label)
+
+      execModel(parameters)
+      with (open(process_path + 'exec_rios_parameters.json', 'w')) as fp:
+          json.dump(parameters, fp)
+      # Save report ipa in BD
+      parse_to_get_ipa_report(out_path,catchment,id_case,id_usuario)
+      headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+      }
+      #------------------------#
+      # TRADUCTOR DE COBERTURAS
+      #------------------------#
+      print ("TRADUCTOR DE COBERTURAS")
+      url = base_url_api + 'cobTrans'
+      first_nbs=nbsList[0] 
+      region = getRegionFromId(basin)
+      region_name = region[4]
+      path_lulc = process_path + 'in/04-RIOS/LULC_%s.tif' % region_name
+      print("path_lulc = %s" % path_lulc) 
+      activity_portfolios_path = process_path + 'out/04-RIOS/1_investment_portfolio_adviser_workspace/activity_portfolios'
+      parameters = {
+          'pathCobs': activity_portfolios_path,
+          'nbs_id': first_nbs,
+          'basin' : basin,
+          'study_case_id' : id_case,
+          'pathLULC': path_lulc
+      }
+      data = makeGetRequest(url, parameters, 5, headers)        
+
+      #-----------------------#
+      #  EJECUCION DE INVEST  #
+      #-----------------------#
+      logger.debug("*** Execute Invest ***")
+      url = base_url_api + 'execInvest'
+      ''' 1. TYPE: CURRENT'''
+      parameters = {
+              'type': 'current',
+              'id_usuario': id_usuario,
+              'basin': basin,
+              'models': ['sdr', 'awy', 'ndr','carbon','swy'],
+              'catchment': catchment,
+              'case': id_case
+      }
+      logger.debug("1. Execute Invest (Current)")
+      try:
+          data_exec_invest_current = makeGetRequest(url, parameters, 5, headers)
+          print(data_exec_invest_current)
+      except requests.exceptions.HTTPError as e:
+          print (e.response.text)
+          logger.warning("error executing::  %s", url)
+
+      #''' 2. type == currentCarbon '''
+      # parameters['type'] = 'currentCarbon'
+
+      # try:
+      # 	data_exec_invest_current_carbon = exec_preproc.makeGetRequest(url,parameters,5,headers)
+      # except:
+      # 	logger.warning("error executing::  %s", url)
+
+      ''' 2. TYPE: BaU '''
+      # campo analysis_period_value de study_cases
+      parameters['type'] = 'BaU'
+      logger.debug("2. Execute Invest (BaU) ")
+      try:
+          data_exec_invest_current = makeGetRequest(url, parameters, 5, headers)
+      except:
+          logger.warning("error executing::  %s", url)
+
+      # ''' Ejecutar Carbon para BaU'''
+
+      ''' 3. TYPE: NBS '''
+      # campo analysis_period_value de study_cases
+      parameters['type'] = 'NBS'
+      try:
+        data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+      except:
+        logger.warning("error executing::  %s", url)
+
+      # ''' Ejecutar Carbon para NBS'''
+
+      # # TODO :: Evaluar si se puede optimizar execInvest adicionando los llamador a 'Carbon' directamente en current, BaU y NBS
+
+      #-------------------------#
+      # EJECUCION DESAGREGACION
+      #-------------------------#
+      url = base_url_api + 'disaggregation'
+      parameters = {
+              'id_usuario': id_usuario,
+              'basin': basin,
+              'catchment': catchment,
+              'case': id_case
+      }
+      try:
+        data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+      except:
+        logger.warning("error executing::  %s", url)     
+      #-------------------------#
+      # EJECUCION WATER BALANCE
+      #-------------------------#
+      url = base_url_api + 'wbdisaggregationIntake'
+      parameters = {
+              'user_id': id_usuario,
+              'id_intake': catchment,
+              'study_case_id': id_case
+      }
+      try:
+        data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+      except:
+        logger.warning("error executing::  %s", url)     
+      #------------------#
+      # EJECUCION ACUEDUCT
+      #------------------#
+      url = base_url_api + 'aqueduct'
+      parameters = {
+              'path': process_path,
+              'id_intake': catchment
+      }
+      try:
+        data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+      except:
+        logger.warning("error executing::  %s", url)    
+  #---------------#
+  # MODELOS PTAP
+  #--------------#
+  for counter, ptap in enumerate(ptapList):
+      ''' 1. WB DISAGGREGATION'''
+      url = base_url_api + 'wbdisaggregationPTAP'
+      parameters = {
+              'ptap_id': ptap,
+              'user_id': id_usuario,
+              'study_case_id': id_case
+      }
+      try:
+        data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+      except:
+        logger.warning("error executing::  %s", url)   
+  #-----------------------------#
+  # EJECUCION FUNCIONES DE COSTO
+  #-----------------------------#
+  url = base_url_api + 'costFunctionExecute'
+  parameters = {
+          'user_id': id_usuario,
+          'intake_id': catchmentList[0],
+          'study_case_id': id_case
+  }
+  try:
+      data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+  except:
+      logger.warning("error executing::  %s", url)  
+  
+  #---------------------#
+  # EJECUCION INDICATORS
+  #---------------------#
+  url = base_url_api + 'indicators'
+  parameters = {
+      'user_id': id_usuario,
+      'study_case_id': id_case
+  }
+  try:
+      data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+  except:
+      logger.warning("error executing::  %s", url)    
+
+  #---------------#
+  # EJECUCION ROI
+  #---------------#
+  url = base_url_api + 'roiExecution'
+  parameters = {
+      'user_id': id_usuario,
+      'study_cases_id': id_case
+  }
+  try:
+      data_exec_invest_current = makeGetRequest(url,parameters,5,headers)
+  except:
+      logger.warning("error executing::  %s", url)    
+
+  updateStudyCaseRunAnalisys(id_case)
+  try:
+      generate_ms_classes(process_path + 'out', activity_portfolios_path)
+  except:
+      logger.warning("error generate_ms_classes::  %s", activity_portfolios_path)
+
+  try:
+      sendEmail(id_usuario, id_case, False)
+  except:
+      logger.warning("error sendEmail::  %s", id_case)
+
+  return result
